38cd97cd9f2a7d08aa44217fab2baa7cfeb541e4
Mina Asham <mina.asham@hotmail.com>
2015-03-10 21:43:00 +0000

    Cleaning up unused imports.

--- algs4/algs4/Whitelist.cs
+++ algs4/algs4/Whitelist.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using algs4.stdlib;
+﻿using algs4.stdlib;
 
 namespace algs4.algs4
 {
