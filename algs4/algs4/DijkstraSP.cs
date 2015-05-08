bd5ce6c4591a472253b0eb75fc45489d9ec9fc64
Mina Asham <mina.asham@hotmail.com>
2015-05-08 21:19:22 +0100

    algs4 addition: - IndexMinPQ - DijkstraSP

---
+++ algs4/algs4/DijkstraSP.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using algs4.stdlib;
+
+namespace algs4.algs4
+{
+    public class DijkstraSP
+    {
+        /// <summary>
+        /// distTo[v] = distance  of shortest s->v path
+        /// </summary>
+        private readonly double[] _distTo;
+
+        /// <summary>
+        /// edgeTo[v] = last edge on shortest s->v path
+        /// </summary>
+        private readonly DirectedEdge[] _edgeTo;
+
+        /// <summary>
+        /// Priority queue of vertices
+        /// </summary>
+        private readonly IndexMinPQ<Double> _pq;
+
+        /// <summary>
+        /// Computes a shortest paths tree from s to every other vertex in
+        /// the edge-weighted digraph G.
+        /// </summary>
+        /// <param name="g">the edge-weighted digraph</param>
+        /// <param name="s">the source vertex</param>
+        public DijkstraSP(EdgeWeightedDigraph g, int s)
+        {
+            foreach (DirectedEdge e in g.Edges())
+            {
+                if (e.Weight() < 0)
+                {
+                    throw new ArgumentException("edge " + e + " has negative weight");
+                }
+            }
+
+            _distTo = new double[g.V()];
+            _edgeTo = new DirectedEdge[g.V()];
+            for (int v = 0; v < g.V(); v++)
+            {
+                _distTo[v] = double.PositiveInfinity;
+            }
+            _distTo[s] = 0.0;
+
+            // relax vertices in order of distance from s
+            _pq = new IndexMinPQ<Double>(g.V());
+            _pq.Insert(s, _distTo[s]);
+            while (!_pq.IsEmpty())
+            {
+                int v = _pq.DelMin();
+                foreach (DirectedEdge e in g.Adj(v))
+                {
+                    Relax(e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Relax edge e and update pq if changed
+        /// </summary>
+        /// <param name="e"></param>
+        private void Relax(DirectedEdge e)
+        {
+            int v = e.From(), w = e.To();
+            if (_distTo[w] > _distTo[v] + e.Weight())
+            {
+                _distTo[w] = _distTo[v] + e.Weight();
+                _edgeTo[w] = e;
+                if (_pq.Contains(w))
+                {
+                    _pq.DecreaseKey(w, _distTo[w]);
+                }
+                else
+                {
+                    _pq.Insert(w, _distTo[w]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the length of a shortest path from the source vertex s to vertex v.
+        /// </summary>
+        /// <param name="v">the destination vertex</param>
+        /// <returns>the length of a shortest path from the source vertex s to vertex v; double.PositiveInfinity if no such path</returns>
+        public double DistTo(int v)
+        {
+            return _distTo[v];
+        }
+
+        /// <summary>
+        /// Is there a path from the source vertex s to vertex v?
+        /// </summary>
+        /// <param name="v">the destination vertex</param>
+        /// <returns>true if there is a path from the source vertex s to vertex v, and false otherwise</returns>
+        public bool HasPathTo(int v)
+        {
+            return _distTo[v] < double.PositiveInfinity;
+        }
+
+        /// <summary>
+        /// Returns a shortest path from the source vertex s to vertex v.
+        /// </summary>
+        /// <param name="v">the destination vertex</param>
+        /// <returns>a shortest path from the source vertex s to vertex v as an iterable of edges, and null if no such path</returns>
+        public IEnumerable<DirectedEdge> PathTo(int v)
+        {
+            if (!HasPathTo(v))
+            {
+                return null;
+            }
+            Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+            for (DirectedEdge e = _edgeTo[v]; e != null; e = _edgeTo[e.From()])
+            {
+                path.Push(e);
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// Check optimality conditions:
+        /// (i) for all edges e:            distTo[e.To()] &lt;= distTo[e.From()] + e.Weight()
+        /// (ii) for all edge e on the SPT: distTo[e.To()] == distTo[e.From()] + e.Weight()
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Check(EdgeWeightedDigraph g, int s)
+        {
+            // check that edge weights are nonnegative
+            foreach (DirectedEdge e in g.Edges())
+            {
+                if (e.Weight() < 0)
+                {
+                    Console.Error.WriteLine("negative edge weight detected");
+                    return false;
+                }
+            }
+
+            // check that distTo[v] and edgeTo[v] are consistent
+            if (Math.Abs(_distTo[s]) > double.Epsilon || _edgeTo[s] != null)
+            {
+                Console.Error.WriteLine("distTo[s] and edgeTo[s] inconsistent");
+                return false;
+            }
+            for (int v = 0; v < g.V(); v++)
+            {
+                if (v == s)
+                {
+                    continue;
+                }
+                if (_edgeTo[v] == null && !double.IsPositiveInfinity(_distTo[v]))
+                {
+                    Console.Error.WriteLine("distTo[] and edgeTo[] inconsistent");
+                    return false;
+                }
+            }
+
+            // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.Weight()
+            for (int v = 0; v < g.V(); v++)
+            {
+                foreach (DirectedEdge e in g.Adj(v))
+                {
+                    int w = e.To();
+                    if (_distTo[v] + e.Weight() < _distTo[w])
+                    {
+                        Console.Error.WriteLine("edge " + e + " not relaxed");
+                        return false;
+                    }
+                }
+            }
+
+            // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.Weight()
+            for (int w = 0; w < g.V(); w++)
+            {
+                if (_edgeTo[w] == null)
+                {
+                    continue;
+                }
+                DirectedEdge e = _edgeTo[w];
+                int v = e.From();
+                if (w != e.To())
+                {
+                    return false;
+                }
+                if (Math.Abs(_distTo[v] + e.Weight() - _distTo[w]) > double.Epsilon)
+                {
+                    Console.Error.WriteLine("edge " + e + " on shortest path not tight");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Unit tests the DijkstraSP data type.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void RunMain(string[] args)
+        {
+            In input = new In(args[0]);
+            EdgeWeightedDigraph g = new EdgeWeightedDigraph(input);
+            int s = int.Parse(args[1]);
+
+            // compute shortest paths
+            DijkstraSP sp = new DijkstraSP(g, s);
+
+            // print shortest path
+            for (int t = 0; t < g.V(); t++)
+            {
+                if (sp.HasPathTo(t))
+                {
+                    StdOut.PrintF("%d to %d (%.2f)  ", s, t, sp.DistTo(t));
+                    if (sp.HasPathTo(t))
+                    {
+                        foreach (DirectedEdge e in sp.PathTo(t))
+                        {
+                            StdOut.Print(e + "   ");
+                        }
+                    }
+                    StdOut.PrintLn();
+                }
+                else
+                {
+                    StdOut.PrintF("%d to %d         no path\n", s, t);
+                }
+            }
+        }
+    }
+}
---
+++ algs4/algs4/IndexMinPQ.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using algs4.stdlib;
+
+namespace algs4.algs4
+{
+    public class IndexMinPQ<TKey> : IEnumerable<int> where TKey : IComparable<TKey>
+    {
+        /// <summary>
+        /// Maximum number of elements on PQ
+        /// </summary>
+        private readonly int _nmax;
+
+        /// <summary>
+        /// Number of elements on PQ
+        /// </summary>
+        private int _n;
+
+        /// <summary>
+        /// Binary heap using 1-based indexing
+        /// </summary>
+        private readonly int[] _pq;
+
+        /// <summary>
+        /// Inverse of pq - qp[pq[i]] = pq[qp[i]] = i
+        /// </summary>
+        private readonly int[] _qp;
+
+        /// <summary>
+        /// keys[i] = priority of i
+        /// </summary>
+        private readonly TKey[] _keys;
+
+        /// <summary>
+        /// Initializes an empty indexed priority queue with indices between 0 and NMAX-1.
+        /// </summary>
+        /// <param name="nmax">the keys on the priority queue are index from 0 to nmax-1</param>
+        public IndexMinPQ(int nmax)
+        {
+            if (nmax < 0)
+            {
+                throw new ArgumentException();
+            }
+            _nmax = nmax;
+            _keys = new TKey[nmax + 1]; // make this of length NMAX??
+            _pq = new int[nmax + 1];
+            _qp = new int[nmax + 1]; // make this of length NMAX??
+            for (int i = 0; i <= nmax; i++)
+            {
+                _qp[i] = -1;
+            }
+        }
+
+        /// <summary>
+        /// Is the priority queue empty?
+        /// </summary>
+        /// <returns>true if the priority queue is empty; false otherwise</returns>
+        public bool IsEmpty()
+        {
+            return _n == 0;
+        }
+
+        /// <summary>
+        /// Is i an index on the priority queue?
+        /// </summary>
+        /// <param name="i">an index</param>
+        /// <returns></returns>
+        public bool Contains(int i)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _qp[i] != -1;
+        }
+
+        /// <summary>
+        /// Returns the number of keys on the priority queue.
+        /// </summary>
+        /// <returns>the number of keys on the priority queue</returns>
+        public int Size()
+        {
+            return _n;
+        }
+
+        /// <summary>
+        /// Associates key with index i.
+        /// </summary>
+        /// <param name="i">an index</param>
+        /// <param name="key">the key to associate with index i</param>
+        public void Insert(int i, TKey key)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (Contains(i))
+            {
+                throw new ArgumentException("index is already in the priority queue");
+            }
+            _n++;
+            _qp[i] = _n;
+            _pq[_n] = i;
+            _keys[i] = key;
+            Swim(_n);
+        }
+
+        /// <summary>
+        /// Returns an index associated with a minimum key.
+        /// </summary>
+        /// <returns>an index associated with a minimum key</returns>
+        public int MinIndex()
+        {
+            if (_n == 0)
+            {
+                throw new InvalidOperationException("Priority queue underflow");
+            }
+            return _pq[1];
+        }
+
+        /// <summary>
+        /// Returns a minimum key.
+        /// </summary>
+        /// <returns>a minimum key</returns>
+        public TKey MinKey()
+        {
+            if (_n == 0)
+            {
+                throw new InvalidOperationException("Priority queue underflow");
+            }
+            return _keys[_pq[1]];
+        }
+
+        /// <summary>
+        /// Removes a minimum key and returns its associated index.
+        /// </summary>
+        /// <returns>an index associated with a minimum key</returns>
+        public int DelMin()
+        {
+            if (_n == 0)
+            {
+                throw new InvalidOperationException("Priority queue underflow");
+            }
+            int min = _pq[1];
+            Exch(1, _n--);
+            Sink(1);
+            _qp[min] = -1; // delete
+            _keys[_pq[_n + 1]] = default(TKey); // to help with garbage collection
+            _pq[_n + 1] = -1; // not needed
+            return min;
+        }
+
+        /// <summary>
+        /// Returns the key associated with index i.
+        /// </summary>
+        /// <param name="i">the index of the key to return</param>
+        /// <returns>the key associated with index i</returns>
+        public TKey KeyOf(int i)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (!Contains(i))
+            {
+                throw new InvalidOperationException("index is not in the priority queue");
+            }
+            return _keys[i];
+        }
+
+        /// <summary>
+        /// Change the key associated with index i to the specified value.
+        /// </summary>
+        /// <param name="i">the index of the key to change</param>
+        /// <param name="key">change the key assocated with index i to this key</param>
+        [Obsolete("Replaced by ChangeKey")]
+        public void Change(int i, TKey key)
+        {
+            ChangeKey(i, key);
+        }
+
+        /**
+         * Change the key associated with index i to the specified value.
+         * @param i the index of the key to change
+         * @param key change the key assocated with index i to this key
+         * @throws java.lang.IndexOutOfRangeException unless 0 &le; i < NMAX
+         * @throws java.util.InvalidOperationException no key is associated with index i
+         */
+
+        public void ChangeKey(int i, TKey key)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (!Contains(i))
+            {
+                throw new InvalidOperationException("index is not in the priority queue");
+            }
+            _keys[i] = key;
+            Swim(_qp[i]);
+            Sink(_qp[i]);
+        }
+
+        /// <summary>
+        /// Decrease the key associated with index i to the specified value.
+        /// </summary>
+        /// <param name="i">the index of the key to decrease</param>
+        /// <param name="key">decrease the key assocated with index i to this key</param>
+        public void DecreaseKey(int i, TKey key)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (!Contains(i))
+            {
+                throw new InvalidOperationException("index is not in the priority queue");
+            }
+            if (_keys[i].CompareTo(key) <= 0)
+            {
+                throw new ArgumentException("Calling decreaseKey() with given argument would not strictly decrease the key");
+            }
+            _keys[i] = key;
+            Swim(_qp[i]);
+        }
+
+        /// <summary>
+        /// Increase the key associated with index i to the specified value.
+        /// </summary>
+        /// <param name="i">the index of the key to increase</param>
+        /// <param name="key">increase the key assocated with index i to this key</param>
+        public void IncreaseKey(int i, TKey key)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (!Contains(i))
+            {
+                throw new InvalidOperationException("index is not in the priority queue");
+            }
+            if (_keys[i].CompareTo(key) >= 0)
+            {
+                throw new ArgumentException("Calling increaseKey() with given argument would not strictly increase the key");
+            }
+            _keys[i] = key;
+            Sink(_qp[i]);
+        }
+
+        /// <summary>
+        /// Remove the key associated with index i.
+        /// </summary>
+        /// <param name="i">the index of the key to remove</param>
+        public void Delete(int i)
+        {
+            if (i < 0 || i >= _nmax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (!Contains(i))
+            {
+                throw new InvalidOperationException("index is not in the priority queue");
+            }
+            int index = _qp[i];
+            Exch(index, _n--);
+            Swim(index);
+            Sink(index);
+            _keys[i] = default(TKey);
+            _qp[i] = -1;
+        }
+
+        #region General helper functions
+
+        private bool Greater(int i, int j)
+        {
+            return _keys[_pq[i]].CompareTo(_keys[_pq[j]]) > 0;
+        }
+
+        private void Exch(int i, int j)
+        {
+            int swap = _pq[i];
+            _pq[i] = _pq[j];
+            _pq[j] = swap;
+            _qp[_pq[i]] = i;
+            _qp[_pq[j]] = j;
+        }
+
+        #endregion
+
+        #region Heap helper functions
+
+        private void Swim(int k)
+        {
+            while (k > 1 && Greater(k / 2, k))
+            {
+                Exch(k, k / 2);
+                k = k / 2;
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (2 * k <= _n)
+            {
+                int j = 2 * k;
+                if (j < _n && Greater(j, j + 1))
+                {
+                    j++;
+                }
+                if (!Greater(k, j))
+                {
+                    break;
+                }
+                Exch(k, j);
+                k = j;
+            }
+        }
+
+        #endregion
+
+        #region Iterators
+
+        /// <summary>
+        /// Returns an iterator that iterates over the keys on the
+        /// priority queue in ascending order.
+        /// The iterator doesn't implement remove() since it's optional.
+        /// </summary>
+        /// <returns>an iterator that iterates over the keys in ascending order</returns>
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new HeapIterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class HeapIterator : IEnumerator<int>
+        {
+            /// <summary>
+            /// Create a new pq
+            /// </summary>
+            private IndexMinPQ<TKey> _copy;
+
+            private IndexMinPQ<TKey> _original;
+
+            public int Current { get; private set; }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            /// <summary>
+            /// Add all elements to copy of heap
+            /// takes linear time since already in heap order so no keys move
+            /// </summary>
+            /// <param name="indexMinPQ"></param>
+            public HeapIterator(IndexMinPQ<TKey> indexMinPQ)
+            {
+                _original = indexMinPQ;
+                Reset();
+            }
+
+            public void Dispose()
+            {
+                _original = null;
+                _copy = null;
+            }
+
+            public bool MoveNext()
+            {
+                bool hasNext = !_copy.IsEmpty();
+                if (hasNext)
+                {
+                    Current = _copy.DelMin();
+                }
+                return hasNext;
+            }
+
+            public void Reset()
+            {
+                _copy = new IndexMinPQ<TKey>(_original._pq.Length - 1);
+                for (int i = 1; i <= _original._n; i++)
+                {
+                    _copy.Insert(_original._pq[i], _original._keys[_original._pq[i]]);
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Unit tests the IndexMinPQ data type.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void RunMain(string[] args)
+        {
+            // insert a bunch of strings
+            string[] strings = { "it", "was", "the", "best", "of", "times", "it", "was", "the", "worst" };
+
+            IndexMinPQ<string> pq = new IndexMinPQ<string>(strings.Length);
+            for (int i = 0; i < strings.Length; i++)
+            {
+                pq.Insert(i, strings[i]);
+            }
+
+            // delete and print each key
+            while (!pq.IsEmpty())
+            {
+                int i = pq.DelMin();
+                StdOut.PrintLn(i + " " + strings[i]);
+            }
+            StdOut.PrintLn();
+
+            // reinsert the same strings
+            for (int i = 0; i < strings.Length; i++)
+            {
+                pq.Insert(i, strings[i]);
+            }
+
+            // print each key using the iterator
+            foreach (int i in pq)
+            {
+                StdOut.PrintLn(i + " " + strings[i]);
+            }
+            while (!pq.IsEmpty())
+            {
+                pq.DelMin();
+            }
+        }
+    }
+}
