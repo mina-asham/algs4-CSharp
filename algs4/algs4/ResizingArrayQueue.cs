6b5113687f05b9a78b95eb8cefea758583a4c86a
Mina Asham <mina.asham@hotmail.com>
2015-05-17 22:23:34 +0100

    Adding ResizingArrayQueue and ResizingArrayStack to algs4

---
+++ algs4/algs4/ResizingArrayQueue.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using algs4.stdlib;
+
+namespace algs4.algs4
+{
+    public class ResizingArrayQueue<TItem> : IEnumerable<TItem>
+    {
+        /// <summary>
+        /// Queue elements
+        /// </summary>
+        private TItem[] _q;
+
+        /// <summary>
+        /// Number of elements on queue
+        /// </summary>
+        private int _n;
+
+        /// <summary>
+        /// Index of first element of queue
+        /// </summary>
+        private int _first;
+
+        /// <summary>
+        /// Index of next available slot
+        /// </summary>
+        private int _last;
+
+        /// <summary>
+        /// Initializes an empty queue.
+        /// </summary>
+        public ResizingArrayQueue()
+        {
+            _q = new TItem[2];
+        }
+
+        /// <summary>
+        /// Is this queue empty?
+        /// </summary>
+        /// <returns>true if this queue is empty; false otherwise</returns>
+        public bool IsEmpty()
+        {
+            return _n == 0;
+        }
+
+        /// <summary>
+        /// Returns the number of items in this queue.
+        /// </summary>
+        /// <returns>the number of items in this queue</returns>
+        public int Size()
+        {
+            return _n;
+        }
+
+        /// <summary>
+        /// Resize the underlying array
+        /// </summary>
+        /// <param name="max"></param>
+        private void Resize(int max)
+        {
+            Debug.Assert(max >= _n);
+            TItem[] temp = new TItem[max];
+            for (int i = 0; i < _n; i++)
+            {
+                temp[i] = _q[(_first + i) % _q.Length];
+            }
+            _q = temp;
+            _first = 0;
+            _last = _n;
+        }
+
+        /// <summary>
+        /// Adds the item to this queue.
+        /// </summary>
+        /// <param name="item">the item to add</param>
+        public void Enqueue(TItem item)
+        {
+            // double size of array if necessary and recopy to front of array
+            if (_n == _q.Length)
+            {
+                Resize(2 * _q.Length); // double size of array if necessary
+            }
+            _q[_last++] = item; // add item
+            if (_last == _q.Length)
+            {
+                _last = 0; // wrap-around
+            }
+            _n++;
+        }
+
+        /// <summary>
+        /// Removes and returns the item on this queue that was least recently added.
+        /// </summary>
+        /// <returns>the item on this queue that was least recently added</returns>
+        public TItem Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue underflow");
+            }
+            TItem item = _q[_first];
+            _q[_first] = default(TItem); // to avoid loitering
+            _n--;
+            _first++;
+            if (_first == _q.Length)
+            {
+                _first = 0; // wrap-around
+            }
+            // shrink size of array if necessary
+            if (_n > 0 && _n == _q.Length / 4)
+            {
+                Resize(_q.Length / 2);
+            }
+            return item;
+        }
+
+        /// <summary>
+        /// Returns the item least recently added to this queue.
+        /// </summary>
+        /// <returns>the item least recently added to this queue</returns>
+        public TItem Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue underflow");
+            }
+            return _q[_first];
+        }
+
+        /// <summary>
+        /// Returns an iterator that iterates over the items in this queue in FIFO order.
+        /// </summary>
+        /// <returns>an iterator that iterates over the items in this queue in FIFO order</returns>
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return new ArrayIterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// An iterator
+        /// </summary>
+        private class ArrayIterator : IEnumerator<TItem>
+        {
+            private int _i;
+            private ResizingArrayQueue<TItem> _resizingArrayQueue;
+
+            public TItem Current { get; private set; }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public ArrayIterator(ResizingArrayQueue<TItem> resizingArrayQueue)
+            {
+                _resizingArrayQueue = resizingArrayQueue;
+                Reset();
+            }
+
+            public bool MoveNext()
+            {
+                bool hasNext = _i < _resizingArrayQueue._n;
+                if (hasNext)
+                {
+                    TItem[] q = _resizingArrayQueue._q;
+                    int first = _resizingArrayQueue._first;
+
+                    Current = q[(_i + first) % q.Length];
+                    _i++;
+                }
+                return hasNext;
+            }
+
+            public void Reset()
+            {
+                _i = 0;
+            }
+
+            public void Dispose()
+            {
+                _resizingArrayQueue = null;
+            }
+        }
+
+        /// <summary>
+        /// Unit tests the ResizingArrayQueue data type.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void RunMain(string[] args)
+        {
+            ResizingArrayQueue<string> q = new ResizingArrayQueue<string>();
+            while (!StdIn.IsEmpty())
+            {
+                string item = StdIn.ReadString();
+                if (item != "-")
+                {
+                    q.Enqueue(item);
+                }
+                else if (!q.IsEmpty())
+                {
+                    StdOut.Print(q.Dequeue() + " ");
+                }
+            }
+            StdOut.PrintLn("(" + q.Size() + " left on queue)");
+        }
+    }
+}
---
+++ algs4/algs4/ResizingArrayStack.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using algs4.stdlib;
+
+namespace algs4.algs4
+{
+    public class ResizingArrayStack<TItem> : IEnumerable<TItem>
+    {
+        /// <summary>
+        /// Array of items
+        /// </summary>
+        private TItem[] _a;
+
+        /// <summary>
+        /// Number of elements on stack
+        /// </summary>
+        private int _n;
+
+        /// <summary>
+        /// Initializes an empty stack.
+        /// </summary>
+        public ResizingArrayStack()
+        {
+            _a = new TItem[2];
+        }
+
+        /// <summary>
+        /// Is this stack empty?
+        /// </summary>
+        /// <returns>true if this stack is empty; false otherwise</returns>
+        public bool IsEmpty()
+        {
+            return _n == 0;
+        }
+
+        /// <summary>
+        /// Returns the number of items in the stack.
+        /// </summary>
+        /// <returns>the number of items in the stack</returns>
+        public int Size()
+        {
+            return _n;
+        }
+
+        /// <summary>
+        /// Resize the underlying array holding the elements
+        /// </summary>
+        /// <param name="capacity"></param>
+        private void Resize(int capacity)
+        {
+            Debug.Assert(capacity >= _n);
+            TItem[] temp = new TItem[capacity];
+            for (int i = 0; i < _n; i++)
+            {
+                temp[i] = _a[i];
+            }
+            _a = temp;
+        }
+
+        /// <summary>
+        /// Adds the item to this stack.
+        /// </summary>
+        /// <param name="item">the item to add</param>
+        public void Push(TItem item)
+        {
+            if (_n == _a.Length)
+            {
+                Resize(2 * _a.Length); // double size of array if necessary
+            }
+            _a[_n++] = item; // add item
+        }
+
+        /// <summary>
+        /// Removes and returns the item most recently added to this stack.
+        /// </summary>
+        /// <returns>the item most recently added</returns>
+        public TItem Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack underflow");
+            }
+            TItem item = _a[_n - 1];
+            _a[_n - 1] = default(TItem); // to avoid loitering
+            _n--;
+            // shrink size of array if necessary
+            if (_n > 0 && _n == _a.Length / 4)
+            {
+                Resize(_a.Length / 2);
+            }
+            return item;
+        }
+
+        /// <summary>
+        /// Returns (but does not remove) the item most recently added to this stack.
+        /// </summary>
+        /// <returns>the item most recently added to this stack</returns>
+        public TItem Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack underflow");
+            }
+            return _a[_n - 1];
+        }
+
+        /// <summary>
+        /// Returns an iterator to this stack that iterates through the items in LIFO order.
+        /// </summary>
+        /// <returns>an iterator to this stack that iterates through the items in LIFO order.</returns>
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return new ReverseArrayIterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// An iterator
+        /// </summary>
+        private class ReverseArrayIterator : IEnumerator<TItem>
+        {
+            private ResizingArrayStack<TItem> _resizingArrayStack;
+            private int _i;
+
+            public TItem Current { get; private set; }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public ReverseArrayIterator(ResizingArrayStack<TItem> resizingArrayStack)
+            {
+                _resizingArrayStack = resizingArrayStack;
+                Reset();
+            }
+
+            public bool MoveNext()
+            {
+                bool hasNext = _i >= 0;
+                if (hasNext)
+                {
+                    Current = _resizingArrayStack._a[_i--];
+                }
+                return hasNext;
+            }
+
+            public void Reset()
+            {
+                _i = _resizingArrayStack._n - 1;
+            }
+
+            public void Dispose()
+            {
+                _resizingArrayStack = null;
+            }
+        }
+
+        /// <summary>
+        /// Unit tests the Stack data type.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void RunMain(string[] args)
+        {
+            ResizingArrayStack<string> s = new ResizingArrayStack<string>();
+            while (!StdIn.IsEmpty())
+            {
+                string item = StdIn.ReadString();
+                if (item != "-")
+                {
+                    s.Push(item);
+                }
+                else if (!s.IsEmpty())
+                {
+                    StdOut.Print(s.Pop() + " ");
+                }
+            }
+            StdOut.PrintLn("(" + s.Size() + " left on stack)");
+        }
+    }
+}
